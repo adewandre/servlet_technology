8501cb2b58ee2598cc77725f37bb32de3a284e03
Nele Custers <custersnele@gmail.com>
2022-05-01 16:44:51 +0200

    update course material

---
+++ src/main/java/be/pxl/paj/servlets/ExampleContextListener.java
@@ -0,0 +1,19 @@
+package be.pxl.paj.servlets;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class ExampleContextListener implements ServletContextListener {
+
+	@Override
+	public void contextInitialized(ServletContextEvent servletContextEvent) {
+		System.out.println("ServletDemo starting up!");
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent servletContextEvent) {
+		System.out.println("ServletDemo shutting down!");
+	}
+}
