128eddb1fa8bb16569a501cfd3c30213dc16d2aa
Nele Custers <custersnele@gmail.com>
2022-05-02 10:29:55 +0200

    update course material

--- src/main/java/be/pxl/paj/servlets/ServletDemoApplication.java
+++ src/main/java/be/pxl/paj/servlets/ServletDemoApplication.java
@@ -2,14 +2,7 @@ package be.pxl.paj.servlets;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Description;
-import org.springframework.web.client.RestTemplate;
-import org.thymeleaf.spring5.SpringTemplateEngine;
-import org.thymeleaf.spring5.view.ThymeleafViewResolver;
-import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 @SpringBootApplication
 @ServletComponentScan
