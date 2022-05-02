351ce16dcec3d71d445b51366a00d7a91ea02534
Nele Custers <custersnele@gmail.com>
2022-05-02 11:26:54 +0200

    fix button click

--- src/main/java/be/pxl/paj/servlets/ButtonClickedServlet.java
+++ src/main/java/be/pxl/paj/servlets/ButtonClickedServlet.java
@@ -15,7 +15,7 @@ public class ButtonClickedServlet extends HttpServlet {
 	@Override
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
-		int clickCount;
+		int clickCount = 0;
 
 		//get the session, which contains user-specific data
 		HttpSession session = request.getSession();
@@ -23,10 +23,6 @@ public class ButtonClickedServlet extends HttpServlet {
 		if(session.getAttribute("clickCount") != null){
 			//we've already stored the clickCount in a previous request, so get it
 			clickCount = (int) session.getAttribute("clickCount");
-		}
-		else{
-			//we haven't stored the clickCount for this user yet, start it at zero
-			clickCount = 0;
 		}
 
 		clickCount++;
@@ -36,6 +32,6 @@ public class ButtonClickedServlet extends HttpServlet {
 
 		//output the clickCount for this user
 		response.getOutputStream().println("<h1>You clicked the button " + clickCount + " times.</h1>");
-		response.getOutputStream().println("<p>Click <a href=\"/index.html\">here</a> to go back to the button.</p>");
+		response.getOutputStream().println("<p>Click <a href=\"/click_the_button.html\">here</a> to go back to the button.</p>");
 	}
 }
