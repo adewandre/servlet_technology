501ed34d3004bfd18a3624b91bfbeb20fe9030cd
Nele Custers <custersnele@gmail.com>
2024-03-11 13:38:55 +0100

    update version and index.html

--- src/main/java/be/pxl/paj/servlets/UsingCookies.java
+++ src/main/java/be/pxl/paj/servlets/UsingCookies.java
@@ -5,6 +5,8 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+import java.awt.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -35,11 +37,9 @@ public class UsingCookies extends HttpServlet {
 			}
 		}
 
-		if (!foundCookie) {
-			Cookie cookie1 = new Cookie("color", "cyan");
-			cookie1.setMaxAge(24 * 60 * 60);
-			response.addCookie(cookie1);
-		}
+		Cookie cookie1 = new Cookie("color", Integer.toHexString((int)(Math.random() * 0x1000000)));
+		cookie1.setMaxAge(24 * 60 * 60);
+		response.addCookie(cookie1);
 
 		out.println(">");
 		out.println("<H1>Setting and Reading Cookies</H1>");
